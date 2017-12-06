7f07b5af115f415f001adfef882e5a6150e3797c
Gy1111 <32634329+Gy1111@users.noreply.github.com>
2017-12-06 14:31:20 +0500

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>Mozilla Drumbeat</title>
+
+<script type="text/javascript">
+
+function HelloWorld()
+{
+	myname = prompt("Where's yer name bub?", "");
+	alert ("Hello" + myname);
+}
+
+</script>
+
+</head>
+<body>
+
+<a href="javascript:HelloWorld()">Hello</a>
+
+</body>
+</html>
